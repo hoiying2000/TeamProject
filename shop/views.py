8a3484131894fcd8fd598a2a5faca24cbd88a6f1
Hoiying Wong <x00163500@mytudublin.ie>
2020-11-23 00:49:22 +0000

    views urls and templates added

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def allProdCat(request, category_id=None):
+    c_page = None
+    products_list = None
+    if category_id !=None:
+        c_page = get_object_or_404(Category, id=category_id)
+        products_list = Product.objects.filter(category=c_page, available=True)
+    else:
+        products_list = Product.objects.all().filter(available=True)
+
+    return render(request, 'shop/category.html', {'category':c_page, 'products':products})
+
+
+def prod_detail(request, category_id, product_id):
+    try:
+        product = Product.objects.get(category_id=category_id, id=product_id)
+    except Exception as e:
+        raise e
+    return render(request, 'shop/product.html', {'product':product})
